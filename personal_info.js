d0dec12d8a4c6de43ba5ac9ac7cdf39eb36a4199
IvanPenyaHuguet <ivanpenyahuguet@gmail.com>
2020-08-04 01:43:12 +0200

    First deploy

---
+++ personal_info.js
@@ -0,0 +1,98 @@
+const user_id = "user_iIP0Txzb6Msi1q9YDJTrG";
+
+const PrivacyPolitic = () => (
+  <article className="text-justify">
+    <h2 className="text-center text-logo3 text-2xl pb-4">
+      Política de privacidad
+    </h2>
+    <section>
+      <h3 className="text-xl">1. Información general</h3>
+      <section className="pl-5 my-6">
+        <p>
+          Charanga al carrer tiene como compromiso respetar la privacidad de los
+          usuarios y proteger sus datos personales. En este texto se busca
+          informar al usuario en cómo se trataran sus datos personales por
+          charanga al carrer (en adelante este sitio web). Antes de proporcionar
+          los datos personales a este sitio web el usuario debe haber leído y
+          comprendido esta política de privacidad.
+          <br />
+        </p>
+        <p>
+          Sin embargo, este sitio web no puede garantizar la inexpugnabilidad de
+          internet, ausencia total de hackers u otros modos fraudulentos de
+          acceso a los datos personales. Siguiendo lo establecido en el artículo
+          4 del RGPD, se entiende por violación de seguridad de los datos
+          personales toda violación de la seguridad que ocasione la alteración
+          ilícita de los datos personales transmitidos, conservados o tratados o
+          la comunicación o acceso no autorizado a dichos datos.
+          <br />
+        </p>
+        <p>
+          El usuario podrá ejercer sus derechos sobre los datos poniéndose en
+          contacto con charanga al carrer.
+        </p>
+      </section>
+    </section>
+    <section>
+      <h3 className="text-xl">2. Ámbito de aplicación</h3>
+      <section className="pl-5 my-6">
+        <p>
+          Esta política de privacidad se aplica a todos los datos personales
+          recopilados en esta página web a través de los formularios existentes.
+          Se considera datos personales la información relativa a una persona
+          que permite identificarla, como el correo electrónico, nombre o número
+          de teléfono.
+          <br />
+        </p>
+        <p>
+          Respetando lo establecido en el artículo 8 del RGPD, en caso de que el
+          usuario sea menor de catorce años de edad será condición obligatoria
+          el consentimiento de sus padres o tutores antes de ceder sus datos
+          personales en este sitio web.
+          <br />
+        </p>
+        <p>
+          La asociación responsable del tratamiento de estos datos es Charanga
+          al carrer.
+          <br />
+        </p>
+        <p>
+          La política de privacidad se rige por el Reglamento (UE) 2016/679 del
+          Parlamento Europe y del Consejo del 27 de abril de 2016, la Ley
+          Orgánica 3/2018 del 5 de diciembre, el Real Decreto 1720/2007 del 21
+          de diciembre y la Ley Orgánica 3/2018, de 5 de diciembre, de
+          Protección de Datos Personales y garantía de los derechos digitales.
+        </p>
+      </section>
+    </section>
+    <section>
+      <h3 className="text-xl">3. Finalidad del tratamiento</h3>
+      <section className="pl-5 my-6">
+        <p>
+          Los datos recopilados a través de este sitio web se utilizarán
+          mayoritariamente para responder a los mensajes enviados por los
+          usuarios en los formularios y poder ofrecerles servicios, también
+          podrán ser utilizados para el envío de información comercial o
+          comunicaciones electrónicas que charanga al carrer considere de
+          interés para sus usuarios y para realizar estudios comerciales o de
+          uso.
+          <br />
+        </p>
+        <p>
+          En ningún caso los datos serán cedidos o vendidos a un tercero y serán
+          retenidos durante el tiempo mínimo para realizar el tratamiento, el
+          tiempo legal establecido o hasta que se solicite su eliminación.
+          Nuestro sitio web puede incluir hipervínculos a otros sitios que no
+          son operados o controlados por este sitio web, este sitio web no se
+          responsabiliza ni garantiza de las políticas de privacidad de tales
+          sitios. Sin embargo, los datos son utilizados de forma indispensable
+          por servicios de terceros como emailjs con tal de poder prestar el
+          servicio de mensajería y con la forma que realiza el host de esta
+          aplicación.
+        </p>
+      </section>
+    </section>
+  </article>
+);
+
+export { user_id, PrivacyPolitic };
