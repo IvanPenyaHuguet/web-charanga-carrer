28839e155916375410d1c7e436b2788d3e52af5d
IvanPenyaHuguet <ivanpenyahuguet@gmail.com>
2020-08-04 11:35:38 +0200

    Solving overlapping in mobile header

--- components/header/headerMobile.js
+++ components/header/headerMobile.js
@@ -9,13 +9,13 @@ import Link from "next/link";
  */
 const HeaderMobile = () => {
   return (
-    <Container className="flex flex-row h-16 items-center justify-around bg-logo1 z-50">
+    <Container className="flex flex-row h-16 items-start xs:justify-start justify-between bg-logo1 z-50">
       <Link href="/" className="">
-        <a className="flex flex-row flex-no-wrap items-center justify-between mr-20 xm:space-x-2 text-logo2">
+        <a className="flex flex-row flex-no-wrap items-center justify-between mr-20 xm:space-x-4 text-logo2 ml-4">
           <div className="pt-2">
             <Logo width="60" height="60" />
           </div>
-          <h1 className="font-semibold text-xl sm:text-2xl h1-font">
+          <h1 className="font-semibold xs:text-xl text-2xl h1-font">
             Charanga Al Carrer
           </h1>
         </a>
