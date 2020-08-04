d49552d37d31d7ad04bf8b246a8fe3533aea96ea
IvanPenyaHuguet <ivanpenyahuguet@gmail.com>
2020-08-04 02:05:27 +0200

    Some changes in mobile

--- components/header/headerMobile.js
+++ components/header/headerMobile.js
@@ -11,15 +11,11 @@ const HeaderMobile = () => {
   return (
     <Container className="flex flex-row h-16 items-center justify-around bg-logo1 z-50">
       <Link href="/">
-        <a>
-          <div className="flex flex-row items-center justify-between space-x-2 text-logo2">
-            <div className="pt-2">
-              <Logo width="60" height="60" />
-            </div>
-            <h1 className="font-semibold text-2xl h1-font">
-              Charanga Al Carrer
-            </h1>
+        <a className="flex flex-row flex-no-wrap items-center justify-between space-x-2 text-logo2">
+          <div className="pt-2">
+            <Logo width="60" height="60" />
           </div>
+          <h1 className="font-semibold text-2xl h1-font">Charanga Al Carrer</h1>
         </a>
       </Link>
       <Nav />
